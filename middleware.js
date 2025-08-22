10cd66d1881a4b839bae5d350804612b2e5dff25
ayanah24 <ayan27452@gmail.com>
2025-08-22 20:07:43 +0530

    handle errmsg problem in middleware.js

--- middleware.js
+++ middleware.js
@@ -9,7 +9,7 @@ function validateListing(req, res, next) {
     let { error } = listingSchema.validate(req.body);
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
+        throw new ExpressError(errMsg , 400);
     } else {
         next();
     }
