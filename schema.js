d087b14c2b25c6084b4c69478ad4ca23d8ce01b6
ayanah24 <ayan27452@gmail.com>
2025-08-22 20:19:02 +0530

    changes to be committed:

--- schema.js
+++ schema.js
@@ -1,14 +1,23 @@
 const Joi = require('joi');
 
+// module.exports.listingSchema = Joi.object({
+//   listing: Joi.object({
+//     title: Joi.string().required(),
+//     description: Joi.string().required(),
+//     location: Joi.string().required(),
+//     country: Joi.string().required(),
+//     price: Joi.number().required().min(0),
+//     images: Joi.any(),      
+//   }).required(),
+//   deleteImages: Joi.array().items(Joi.string()).optional()
+// });
 module.exports.listingSchema = Joi.object({
-  listing: Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    location: Joi.string().required(),
-    country: Joi.string().required(),
-    price: Joi.number().required().min(0),
-    images: Joi.any(),      
-  }).required(),
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  location: Joi.string().required(),
+  country: Joi.string().required(),
+  price: Joi.number().required().min(0),
+  images: Joi.any(),
   deleteImages: Joi.array().items(Joi.string()).optional()
 });
 
